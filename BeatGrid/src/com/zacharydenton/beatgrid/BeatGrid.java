0b754f1b4ff3a6da3f17bfc07f595560c2ef173a
Zach Denton <zacharydenton@gmail.com>
2011-10-13 00:53:23 -0400

    initial commit

---
+++ BeatGrid/src/com/zacharydenton/beatgrid/BeatGrid.java
@@ -0,0 +1,13 @@
+package com.zacharydenton.beatgrid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BeatGrid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
